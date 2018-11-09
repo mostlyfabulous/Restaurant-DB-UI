fe0ba96e3f868d39d562821ac5a50ceb11b0fbbd
Andrew Wong <andrewzywong@alumni.ubc.ca>
2018-11-08 22:45:03 -0800

    Links styled same way as buttons
    Index has css applied as well

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<PHP> <link rel="stylesheet" type="text/css" href="enjoy.css"></head> <PHP>
 <a href="Restaurant.php">Restaurant page</a>
 <a href="Restaurant.php">Chef page</a>
 <a href="Customer.php">Customer page</a>
