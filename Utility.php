332fba1798833d583150cc1d9a5fa7751f77a7a5
Andrew Wong <andrewzywong@alumni.ubc.ca>
2019-08-27 09:58:48 -0700

    Connect

--- Utility.php
+++ Utility.php
@@ -3,8 +3,8 @@ PostgreSQL by Andrew Wong-->
 
 <?php
 $success = True; //keep track of errors so it redirects the page only if there are no errors
-// $db_conn = pg_connect(getenv("DATABASE_URL"));
-$db_conn = pg_connect("host=localhost port=5432 dbname=Andrew user=Andrew" ) or die("Could not connect");
+$db_conn = pg_connect(getenv("DATABASE_URL"));
+// $db_conn = pg_connect("host=localhost port=5432 dbname=Andrew user=Andrew" ) or die("Could not connect");
 $stat = pg_connection_status($db_conn);
   if ($stat === PGSQL_CONNECTION_OK) {
       echo '<h>Connection status ok</h><br><br>';
